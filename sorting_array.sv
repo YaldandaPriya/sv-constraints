ac8b07a3487c3b67e80818de76db27a7b803cb11
Yaldanda Priya <146719581+YaldandaPriya@users.noreply.github.com>
2026-07-01 22:26:16 +0530

    Update sorting_array.sv

--- sorting_array.sv
+++ sorting_array.sv
@@ -1,11 +1,10 @@
-
 //sorting array without using built-in sort function
 class sort_array;
 	rand bit [4:0] a[];
 	rand bit [4:0] size_a;
 
 	constraint array_size{size_a >=2;a.size==size_a;}
-	
+
 	function void post_randomize();
 		int temp;
 		for(int i=0;i<size_a-1;i=i+1)
@@ -38,3 +37,18 @@ begin
 	end
 end
 endmodule
+
+/* OUTPUT
+xcelium> run
+          1-random
+ 1       3       3       4       5       5       5       6       8       8       9      10      11      14      15      15      18      22      23      27      29      31
+          2-random
+ 6       8      15      19      23      27      27      27
+          3-random
+ 3       9      13      18      20
+          4-random
+ 3       5       9      10      11      12      13      14      14      17      17      19      20      21      21      23      24      25      25      25      27      27   28       28      29      30      30
+          5-random
+ 1       1       1       1       2      10      12      13      14      18      18      19      23      24      24      25      25      26      27      30
+xmsim: *W,RNQUIE: Simulation is complete.
+*/
